57aa7bf2ce8a5fc85fdcf441fc10beffd9b3eb4b
Jani Koponen <mr.gravez@gmail.com>
2019-09-23 13:50:15 +0300

    osa2 part 2.6 finished

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import Entry from './components/Entry'
 
-function App() {
+const App = () => {
+  const [ persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [ newName, setNewName ] = useState('')
+
+  const rows = () => persons.map(person => <Entry key={person.name} person={person.name} />)
+
+  const addNewPerson = (event) => {
+  // '${newName} is already added to phonebook'
+
+    event.preventDefault()
+    console.log('Lisätään uusi nimi:', event.target.value)
+    const personObject = {
+      name: newName,
+    }
+
+    console.log('Luotu:', personObject)
+    setPersons(persons.concat(personObject))
+    setNewName('')
+  }
+  
+
+  const handleNewPerson = (event) => {
+    console.log('Syötetään uutta nimeä:', event.target.value)
+    setNewName(event.target.value)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addNewPerson}>
+        <div>
+          name: <input value={newName} onChange={handleNewPerson}/>
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {rows()}
     </div>
-  );
+  )
+
 }
 
-export default App;
+export default App
---
+++ osa2/puhelinluettelo/src/components/Entry.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Entry = ({ person }) => {
+    return(
+        <p>{person}</p>
+    )
+}
+
+export default Entry
