d5839a19bb9a77f115e121fc61b5ed706ebd46eb
Jani Koponen <mr.gravez@gmail.com>
2019-09-23 12:52:38 +0300

    ADD osa2

---
+++ osa2/kurssitiedot/src/components/Course.js
@@ -0,0 +1,34 @@
+import React from 'react'
+
+const Header = ({ name }) => <div><h1>{name}</h1></div>
+
+const Course = ({ course }) => course.map(course => 
+        <div key={Object.keys(course.parts)}>			
+        <Header name={course.name}/>
+        <Content parts={course.parts}/>
+        <Total parts={course.parts}/>	
+        </div>			
+    )					
+
+
+const Content = ({ parts }) => parts.map((parts, i) =>
+        <dl key={i}>
+        <Part part={parts.name} number={parts.exercises}/>
+        </dl>
+)
+
+const Part = ({ part, number }) => <dt>{part} {number}</dt>
+
+const Total = ({ parts }) => {
+    const total = parts.reduce((s, p) => s + p.exercises, 0)
+
+    console.log(parts)
+    return (
+        <div>
+        <p><b>total of {total} exercises </b></p>
+        </div>
+    )
+}
+
+export default Course
+
