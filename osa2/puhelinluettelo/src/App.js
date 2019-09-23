7491c43a7d3ffd1a65d8ed2f193f7a7c4f56ab52
Jani Koponen <mr.gravez@gmail.com>
2019-09-23 15:04:59 +0300

    UPDATE osa2/puhelinluettelo step 2.9 complete

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -3,15 +3,21 @@ import Entry from './components/Entry'
 
 const App = () => {
   const [ persons, setPersons] = useState([
-    { 
-      name: 'Arto Hellas',
-      number: '040-1234567'
-    }
+    { name: 'Arto Hellas', number: '040-1234567' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
   ]) 
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
+  const [ filter, setFilter ] = useState('')
+  const [ match, setMatch ] = useState(false)
 
-  const rows = () => persons.map(person => <Entry key={person.name} name={person.name} number={person.number} />)
+  const numbersToShow = match
+    ? persons
+    : persons.filter(person => person.name.toLowerCase().includes(filter.toLowerCase()))
+
+  const rows = () => numbersToShow.map(person => <Entry key={person.name} name={person.name} number={person.number} />)
 
   const addNewPerson = (event) => {
   // '${newName} is already added to phonebook'
@@ -44,9 +50,17 @@ const App = () => {
     setNewNumber(event.target.value)
   }
 
+  const handleFilter = (event) => {
+    setFilter(event.target.value)
+  }
+
   return (
     <div>
-      <h2>Phonebook</h2>
+      <h1>Phonebook</h1>
+      <div>
+        filter shown with <input value={filter}  onChange={handleFilter}/>
+      </div>
+      <h2>add a new</h2>
       <form onSubmit={addNewPerson}>
         <div>
           name: <input value={newName} onChange={handleNewPerson}/>
