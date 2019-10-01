2d1066c20844cc2208e61564267e99387852d500
k0psutin <mr.gravez@gmail.com>
2019-10-01 19:22:44 +0300

    4.3 done

--- osa4/blogilista/controllers/blogs.js
+++ osa4/blogilista/controllers/blogs.js
@@ -1,22 +1,40 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 
-blogsRouter.get('/', (request, response) => {
-    Blog
-        .find({})
-        .then(blogs => {
-            response.json(blogs)
-        })
+blogsRouter.get('/', (request, response, next) => {
+  Blog
+    .find({})
+    .then(blogs => {
+      response.json(blogs)
+    })
+    .catch(error => next(error))
 })
 
-blogsRouter.post('/', (request, response) => {
-    const blog = new Blog(request.body)
+blogsRouter.get('/:id', (request, response, next) => {
+  Blog.findById(request.params.id)
+    .then((blog) => {
+      response.toJSON(blog)
+    })
+    .catch(error => next(error))
+})
 
-    blog
-        .save()
-        .then(result => {
-            response.status(201).json(result)
-        })
+blogsRouter.delete('/:id', (request, response, next) => {
+  Blog.findByIdAndDelete(request.params.id)
+    .then(() => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
+})
+
+blogsRouter.post('/', (request, response, next) => {
+  const blog = new Blog(request.body)
+
+  blog
+    .save().then(savedBlog => savedBlog.toJSON())
+    .then(savedAndFormattedBlog => {
+      response.json(savedAndFormattedBlog)
+    })
+    .catch(error => next(error))
 })
 
 module.exports = blogsRouter
--- osa4/blogilista/utils/middleware.js
+++ osa4/blogilista/utils/middleware.js
@@ -1,36 +1,41 @@
-const requestLogger = (request, response, next) => {
-    console.log('Method:', request.method)
-    console.log('Path:  ', request.path)
-    console.log('Body:  ', request.body)
-    console.log('---')
-    next()
-}
+const morgan = require('morgan')
+
+const stringFormat = ':method :url :status :req[content-length] :response-time ms - :post'
+
+const requestLogger = morgan(stringFormat)
+
+morgan.token('post', function (req, res) {
+  //console.log(`morgan.token('post'):`, req.body)
+  if(req.body.author != undefined | req.body.url != undefined | req.body.likes != undefined) {
+    return JSON.stringify({ author: req.body.author, url: req.body.url, likes: req.body.likes })
+  }
+})
 
 const unknownEndpoint = (request, response) => {
-    response.status(404).send({
-        error: 'unknown endpoint'
-    })
+  response.status(404).send({
+    error: 'unknown endpoint'
+  })
 }
 
 const errorHandler = (error, request, response, next) => {
-    console.error(error.message)
+  console.error(error.message)
 
-    if (error.name === 'CastError' && error.kind === 'ObjectId') {
-        return response.status(400).send({
-            error: 'malformatted id'
-        })
-    } else if (error.name === 'ValidationError') {
-        return response.status(400).json({
-            error: error.message
-        })
-    }
+  if (error.name === 'CastError' && error.kind === 'ObjectId') {
+    return response.status(400).send({
+      error: 'malformatted id'
+    })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({
+      error: error.message
+    })
+  }
 
-    next(error)
+  next(error)
 }
 
 
 module.exports = {
-    requestLogger,
-    unknownEndpoint,
-    errorHandler
+  requestLogger,
+  unknownEndpoint,
+  errorHandler,
 }
