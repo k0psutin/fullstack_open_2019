38b6caecb87e1fcd79eb6490b258fa1371b5655a
k0psutin <mr.gravez@gmail.com>
2019-10-03 13:30:48 +0300

    4.7 done

--- osa4/blogilista/utils/list_helper.js
+++ osa4/blogilista/utils/list_helper.js
@@ -1,7 +1,60 @@
+const _ = require('lodash')
+
 const dummy = (blogs) => {
   return 1
 }
 
-module.exports = {
-  dummy
+
+const mostFavoriteBlog = (blogs) => {
+  const favoriteBlog = _.maxBy(blogs, 'likes')
+  //const mostLikes = blogs.reduce((sum, blog) => sum + blog.likes, 0)
+  const foundBlog = [
+    {
+      title: favoriteBlog.title,
+      author: favoriteBlog.author,
+      likes: favoriteBlog.likes
+    }
+  ]
+
+  return foundBlog
+} 
+
+const totalLikes = (blogs) => blogs.reduce((sum, blog) => sum + blog.likes, 0)
+
+const mostLikes = (blogs) => _(blogs)
+  .groupBy('author')
+  .map((obj, key) => {
+    return {
+      'author': key,
+      'likes': _.sumBy(obj, 'likes')
+    }
+  })
+  .maxBy('likes')
+
+
+
+const mostBlogs = (blogs) => {
+  
+  const mostDone = _.maxBy(blogs, 'author').author
+  const writtenBlogs = _.countBy(blogs, 'author')
+
+  const mostActive = [
+    {
+      author: mostDone,
+      blogs: writtenBlogs[mostDone]
+    }
+  ]
+
+  return mostActive
 }
+  
+
+module.exports = {
+  dummy,
+  totalLikes,
+  mostLikes,
+  mostBlogs,
+  mostFavoriteBlog
+}
+
+
