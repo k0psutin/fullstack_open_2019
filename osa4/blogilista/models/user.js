4da0f85b0aa3be71d6b1f78e4b516efbd084a5cd
k0psutin <mr.gravez@gmail.com>
2019-10-04 11:36:49 +0300

    4.17 done

--- osa4/blogilista/models/user.js
+++ osa4/blogilista/models/user.js
@@ -11,7 +11,13 @@ const userSchema = mongoose.Schema({
   name: {
     type: String
   },
-  passwordHash: String
+  passwordHash: String,
+  blogs: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Blog'
+    }
+  ]
 })
 
 userSchema.set('toJSON', {
