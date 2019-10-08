7f105dbdea3cb01b53444abf2512cf7a97d743b0
k0psutin <mr.gravez@gmail.com>
2019-10-08 09:37:32 +0300

    6.8 done

--- osa6/redux-anecdotes/src/App.js
+++ osa6/redux-anecdotes/src/App.js
@@ -1,43 +1,12 @@
 import React from 'react'
-import { voteAnecdote } from './reducers/anecdoteReducer'
-import { createAnecdote } from './reducers/anecdoteReducer'
+import AnecdoteForm from './components/AnecdoteForm'
+import AnecdoteList from './components/AnecdoteList'
 
 const App = props => {
-  const anecdotes = props.store.getState()
-  const store = props.store
-
-  const vote = id => {
-    store.dispatch(voteAnecdote(id))
-  }
-
-  const newAnecdote = event => {
-    event.preventDefault()
-    const content = event.target.anecdote.value
-    props.store.dispatch(createAnecdote(content))
-  }
-
   return (
     <div>
-      <h2>Anecdotes</h2>
-      {anecdotes
-        .slice()
-        .sort((a, b) => b.votes - a.votes)
-        .map(anecdote => (
-          <div key={anecdote.id}>
-            <div>{anecdote.content}</div>
-            <div>
-              has {anecdote.votes}
-              <button onClick={() => vote(anecdote.id)}>vote</button>
-            </div>
-          </div>
-        ))}
-      <h2>create new</h2>
-      <form onSubmit={newAnecdote}>
-        <div>
-          <input name='anecdote' />
-        </div>
-        <button type='submit'>create</button>
-      </form>
+      <AnecdoteList store={props.store} />
+      <AnecdoteForm store={props.store} />
     </div>
   )
 }
